3f1733208a417f165170ec2b190f720f1168fa0d
sureshjoshi88 <sj3795789@gmail.com>
2025-08-15 21:14:21 +0530

    updates

--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from '../organisms/Navbar'
+import { Outlet } from 'react-router'
+import Footer from '../organisms/Footer'
 
 const Layout = () => {
   return (
     <div>
-      
+      <header>
+        <Navbar/>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+      <footer>
+    <Footer/>
+      </footer>
     </div>
   )
 }
