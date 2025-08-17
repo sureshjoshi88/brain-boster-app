8f239741eee923e09f382592e7328c7c6ae0d4ad
sureshjoshi88 <sj3795789@gmail.com>
2025-08-17 16:11:11 +0530

    updating

--- src/components/templates/Profile.jsx
+++ src/components/templates/Profile.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-    <div>
-      <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+    <div className='flex justify-center items-center h-100'>
+
+   <div>
+     <div>
+      <img className='h-30 w-30' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+     <input type="file" name="" id="" className='border rounded-full h-10 w-10' />
     </div>
     <div>
-      <p>Name</p>
-      <p> Email</p>
-      <p>Password</p>
+      <p className='font-medium text-xl'>Name</p>
+      <p className='font-medium text-xl'> Email</p>
+      <p className='font-medium text-xl'>Password</p>
     </div>
+   </div>
       
     </div>
   )
