5e039c34a4d1489620fa57367a20c2b6c22ef35f
sureshjoshi88 <sj3795789@gmail.com>
2025-08-17 14:19:57 +0530

    add new content for profile page

--- src/components/templates/Profile.jsx
+++ src/components/templates/Profile.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
+    <div>
+      <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+    </div>
+    <div>
+      <p>Name</p>
+      <p> Email</p>
+      <p>Password</p>
+    </div>
       
     </div>
   )
