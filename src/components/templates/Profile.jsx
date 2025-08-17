a276a3bfc2d35bd6f211166ec8544ff6f6b0ba65
sureshjoshi88 <sj3795789@gmail.com>
2025-08-17 16:19:21 +0530

    add new img feature

--- src/components/templates/Profile.jsx
+++ src/components/templates/Profile.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Profile = () => {
+  const [user,setUser] = useState("https://cdn-icons-png.flaticon.com/512/149/149071.png")
+  const handleimg = (e)=>{
+    const file = e.target.files[0];
+    if(file){
+      const imgurl = URL.createObjectURL(file)
+      setUser(imgurl)
+    }
+  }
   return (
     <div className='flex justify-center items-center h-100'>
 
    <div>
      <div>
-      <img className='h-30 w-30' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-     <input type="file" name="" id="" className='border rounded-full h-10 w-10' />
+      <img className='h-30 w-30' src={user} alt="user" />
+     <input type="file" accept='image/*' onChange={handleimg} name="" id="" className='border rounded-full h-10 w-10' />
     </div>
     <div>
       <p className='font-medium text-xl'>Name</p>
