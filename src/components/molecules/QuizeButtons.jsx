486f5e32c60d831b698d029d4ea4802dbbc85f32
sureshjoshi88 <sj3795789@gmail.com>
2025-08-22 11:03:09 +0530

    update

--- src/components/molecules/QuizeButtons.jsx
+++ src/components/molecules/QuizeButtons.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const QuizeButtons = () => {
-  return (
-    <div>
-      <div className="flex justify-end">
-  <div className="flex gap-6 p-2 mt-5 ">
-   <Link to='/live'> <button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white  border-gray-400  transition duration-300 ease-in-out">Live Quize</button></Link>
-    <Link to='/scheduled'><button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white   border-gray-400  transition duration-300 ease-in-out">Scheduled Quize</button></Link>
- <Link to='/history'>   <button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white   border-gray-400  transition duration-300 ease-in-out">History</button></Link>
-    </div>
-</div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="flex justify-end">
+                <div className="flex gap-6 p-2 mt-5 ">
+                    <Link to='/live'> <button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white  border-gray-400  transition duration-300 ease-in-out">Live Quize</button></Link>
+                    <Link to='/scheduled'><button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white   border-gray-400  transition duration-300 ease-in-out">Scheduled Quize</button></Link>
+                    <Link to='/history'>   <button className="  p-1.5 rounded-4xl ps-3 pe-3 border cursor-pointer hover:bg-blue-600 hover:text-white   border-gray-400  transition duration-300 ease-in-out">History</button></Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default QuizeButtons
