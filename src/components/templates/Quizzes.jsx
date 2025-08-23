b1ca64d5346d0e61f2cba1854a5e62498a70e7ae
sureshjoshi88 <sj3795789@gmail.com>
2025-08-23 10:02:56 +0530

    update

--- src/components/templates/Quizzes.jsx
+++ src/components/templates/Quizzes.jsx
@@ -242,26 +242,7 @@ const Quizzes = () => {
                   </div>
                 </>
               ) : (
-                // <div className="text-center">
-                //   <h2 className="text-2xl font-bold mb-4">🎉 Quiz Completed! 🎉</h2>
-                //   <p className="text-lg mb-2">Your Score: {score} / {currentArray.length}</p>
-                //   <div className="flex justify-between ">
-                //     <button
-                //       onClick={() => { dispatch(resetquize()), startTime() }}
-                //       className="px-4 py-2 bg-blue-600 text-white rounded font-medium cursor-pointer"
-                //     >
-                //       Restart Quiz
-                //     </button>
-                //     <button
-                //       onClick={() => setStarts(false)}
-                //       className="px-4 py-2 bg-blue-600 text-white rounded font-medium cursor-pointer"
-                //     >
-                //       Back to home
-                //     </button>
-                //         {dispatch(addToHistory())}
-
-                //   </div>
-                // </div>
+               
                 <div>
                   <QuizeResult score = {score} currentArray={currentArray} resetquize = {resetquize} startTime={startTime} setStarts={setStarts} addToHistory={addToHistory} />
                 </div>
