6a8a15ca4275c3cc04be64c8a60683735444982c
sureshjoshi88 <sj3795789@gmail.com>
2025-08-23 10:59:59 +0530

    add new files

---
+++ src/components/molecules/quize/LiveQuize.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LiveQuize = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LiveQuize
---
+++ src/data/DummyData.jsx
@@ -0,0 +1,67 @@
+ const subjects = [
+  {
+    id: "javascript",
+    name: "JavaScript",
+    image: "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    description: "Test your JavaScript knowledge!",
+    type: "live",
+  },
+  {
+    id: "html",
+    name: "HTML",
+    image: "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+    description: "Explore your HTML skills!",
+    type: "live",
+  },
+  {
+    id: "react",
+    name: "React",
+    image: "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+    description: "Master your React fundamentals!",
+    type: "scheduled",
+  },
+  {
+    id: "css",
+    name: "CSS",
+    image: "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+    description: "Style your knowledge with CSS!",
+    type: "live",
+  },
+  {
+    id: "nodejs",
+    name: "Node.js",
+    image: "https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg",
+    description: "Check your backend basics with Node.js!",
+    type: "scheduled",
+  },
+  {
+    id: "mongodb",
+    name: "MongoDB",
+    image: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg",
+    description: "Explore your NoSQL skills with MongoDB!",
+    type: "scheduled",
+  },
+  {
+    id: "expressjs",
+    name: "Express.js",
+    image: "https://cdn.worldvectorlogo.com/logos/express-109.svg",
+    description: "How well do you know Express.js?",
+    type: "live",
+  },
+  {
+    id: "typescript",
+    name: "TypeScript",
+    image: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+    description: "Put your TypeScript typing to the test!",
+    type: "scheduled",
+  },
+  {
+    id: "github",
+    name: "GitHub",
+    image: "https://cdn.worldvectorlogo.com/logos/github-icon-1.svg",
+    description: "Version control mastery starts here!",
+    type: "live",
+  },
+];
+
+export default subjects;
