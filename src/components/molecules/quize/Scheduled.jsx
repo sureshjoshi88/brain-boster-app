3d0527330d62831de54100a792403cd66adca458
sureshjoshi88 <sj3795789@gmail.com>
2025-08-24 13:38:39 +0530

    update code

--- src/components/molecules/quize/Scheduled.jsx
+++ src/components/molecules/quize/Scheduled.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import subjects from '../../../data/DummyData';
 
 const Scheduled = () => {
+    const scheduleQuize = subjects.filter((sub)=>sub.type==='scheduled')
   return (
     <div>
       <div className='grid md:grid-cols-3 gap-4 pb-8'>
-            {liveSubject.map((subj) => (
+            {scheduleQuize.map((subj) => (
               <div key={subj.id} className="bg-white shadow-xl rounded-xl p-6 flex flex-col items-center text-center mt-2">
                 <img src={subj.image} alt={subj.name} className="h-20 mb-4" />
                 <h2 className="text-xl font-semibold mb-2">{subj.name}</h2>
