6f81b42ce79a5c2f0e43951404e24a9aa9c2afa1
sureshjoshi88 <sj3795789@gmail.com>
2025-09-08 11:06:40 +0530

    add  new button for blog page add blog

--- src/components/templates/Blog.jsx
+++ src/components/templates/Blog.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Blog = () => {
   return (
     <div>
-      
+        <div>
+            <button className='ps-2 pe-2 p-1 rounded bg-blue-500 text-white font-semibold m-3'>Add Blog</button>
+        </div>
+      <h1>jai shree ram</h1>
     </div>
   )
 }
