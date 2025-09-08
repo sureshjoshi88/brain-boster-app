08307c890f252d36be1a945d0db027e3f9e1c00a
sureshjoshi88 <sj3795789@gmail.com>
2025-09-08 11:37:30 +0530

    update

--- src/components/templates/Blog.jsx
+++ src/components/templates/Blog.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BlogData } from '../../data/DashBorddata'
+import LatestfromBlog from '../molecules/dashBoard/LatestfromBlog'
 
 const Blog = () => {
   return (
@@ -6,7 +8,24 @@ const Blog = () => {
         <div>
             <button className='ps-2 pe-2 p-1 rounded bg-blue-500 text-white font-semibold m-3'>Add Blog</button>
         </div>
-      <h1>jai shree ram</h1>
+      
+      <div>
+        
+        <div>
+            <form action="">
+                
+            </form>
+        </div>
+
+         <div className="p-4 mt-4  rounded-1">
+                            <h2 className="text-xl font-bold mb-4">Latest from Blog</h2>
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                {BlogData.map((item) => (
+                                    <LatestfromBlog key={item.id} item={item} />
+                                ))}
+                            </div>
+                        </div>
+      </div>
     </div>
   )
 }
