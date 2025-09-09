3b4d42fc529cd06ab10e05817b249662f36aa657
sureshjoshi88 <sj3795789@gmail.com>
2025-09-09 12:54:34 +0530

    add new functioin

--- src/components/templates/Blog.jsx
+++ src/components/templates/Blog.jsx
@@ -3,7 +3,20 @@ import { BlogData } from '../../data/DashBorddata'
 import LatestfromBlog from '../molecules/dashBoard/LatestfromBlog'
 
 const Blog = () => {
-  const [form,setForm] = useState(title="",heading="",description="",img=null)
+  const [form,setForm] = useState({title:"",heading:"",description:"",img:""});
+  
+  const handleForm = (e)=>{
+    e.preventDefault()
+    console.log(form);
+    setForm({title:"",heading:"",description:"",img:""})
+
+  }
+  
+  const handleChange=(e)=>{
+    setForm({...form,[e.target.name]:e.target.value})
+  }
+
+
   return (
     <div>
         <div>
@@ -13,13 +26,13 @@ const Blog = () => {
       <div>
         
         <div>
-            <form className='flex justify-center'>
+            <form className='flex justify-center' onSubmit={handleForm}>
               <div className='shadow-2xl shadow-gray-300 p-4 rounded space-y-4'>
-                  <input className='border w-100 rounded-full p-2 outline-0' onChange={(e)=>setForm(e.target.value)} required type="text" name="" id="title"  placeholder='Enter your title'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0'  onChange={(e)=>setForm(e.target.value)} required type="text" name="" id="title"  placeholder='Enter your heading'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0'  onChange={(e)=>setForm(e.target.value)} required type="file" name="" id="title"  placeholder='Enter your title'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0'  onChange={(e)=>setForm(e.target.value)} required name="" id=""placeholder='Enter your description' ></input><br/>
-                <button className='text-white font-semibold rounded-full bg-blue-500  w-100 p-2' type='submit'>Submit</button>
+                  <input className='border w-100 rounded-full p-2 outline-0'  value={form.title} onChange={handleChange} required type="text" name="title" id="title"  placeholder='Enter your title'/><br/>
+                <input className='border w-100 rounded-full p-2 outline-0' value={form.heading}  onChange={handleChange} required type="text" name="heading" id="title"  placeholder='Enter your heading'/><br/>
+                <input className='border w-100 rounded-full p-2 outline-0' value={form.img}  onChange={handleChange} required type="file" name="img" id="title"  placeholder='Enter your title'/><br/>
+                <input className='border w-100 rounded-full p-2 outline-0' value={form.description}  onChange={handleChange} required name="description" id="" placeholder='Enter your description' ></input><br/>
+                <button className='text-white font-semibold rounded-full bg-blue-500 w-100 p-2' >Submit</button>
               </div>
             </form>
         </div>
