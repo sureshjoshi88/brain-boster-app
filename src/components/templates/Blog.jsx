ca6217479061cb806a0bba092cca83f868f7ed80
sureshjoshi88 <sj3795789@gmail.com>
2025-09-10 12:14:15 +0530

    update

--- src/components/templates/Blog.jsx
+++ src/components/templates/Blog.jsx
@@ -3,14 +3,25 @@ import { BlogData } from '../../data/DashBorddata'
 import LatestfromBlog from '../molecules/dashBoard/LatestfromBlog'
 
 const Blog = () => {
-  const [title,setTitle] = useState('')
-  const [heading,setHeading] = useState('')
-  const [description,setDescription] = useState('')
-  const [img,setImg] = useState('')
-  const [formstate,setFormstate] = useState(false)
-  const [array,setArray] = useState([]);
-  
-  const handleForm = (e)=>{
+  const [title, setTitle] = useState('')
+  const [heading, setHeading] = useState('')
+  const [description, setDescription] = useState('')
+  const [img, setImg] = useState('')
+  const [formstate, setFormstate] = useState(false)
+  const [array, setArray] = useState([]);
+
+ const handleImageChange = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImg(reader.result); // base64 string aa jayegi
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+
+  const handleForm = (e) => {
     e.preventDefault()
     const data = {
       title,
@@ -18,66 +29,56 @@ const Blog = () => {
       description,
       img
     }
-   
 
-    
-const handleImageChange = (e) => {
-  const file = e.target.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setImg(reader.result); // base64 string aa jayegi
-    };
-    reader.readAsDataURL(file);
-  }
-}
-
-   const allblog =  [...array ,data];
-   setArray(allblog)
-   localStorage.setItem("brainblog",JSON.stringify(array))
+    const allblog = [...array, data];
+    setArray(allblog)
+    localStorage.setItem("brainblog", JSON.stringify(array))
     setTitle("")
-    setImg(null)
+    setImg("")
     setHeading("")
     setDescription("")
-  
   }
-  
-  useEffect(()=>{
-    const allBlog = JSON.parse(localStorage.getItem("brainblog"))||[];
-    console.log(allBlog);
-    
-  },[])
- 
 
- 
+  
+   
+  console.log(array);
+  
+  useEffect(() => {
+    const allBlog = JSON.parse(localStorage.getItem("brainblog")) || [];
+    console.log(allBlog);
+  }, [array])
+
+
+
   return (
     <div>
-        <div>
-            <button onClick={()=>setFormstate((pre)=>!pre)} className='ps-2 pe-2 p-1 rounded bg-blue-500 text-white font-semibold m-3 cursor-pointer'>Add Blog</button>
-        </div>
-       
       <div>
-        
-      {formstate&&  <div>
-            <form className='flex justify-center' onSubmit={handleForm}>
-              <div className='shadow-2xl shadow-gray-300 p-4 rounded space-y-4'>
-                  <input className='border w-100 rounded-full p-2 outline-0'  value={title} onChange={(e)=>setTitle(e.target.value)} required type="text" name="title" id="title"  placeholder='Enter your title'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0' value={heading}  onChange={(e)=>setHeading(e.target.value)} required type="text" name="heading" id="title"  placeholder='Enter your heading'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0' value={img}  onChange={(e)=>setImg(e.target.value)} required type="file" name="img" id="title"  placeholder='Enter your title'/><br/>
-                <input className='border w-100 rounded-full p-2 outline-0' value={description}  onChange={(e)=>setDescription(e.target.value)} required name="description" id="" placeholder='Enter your description' ></input><br/>
-                <button className='text-white font-semibold rounded-full bg-blue-500 w-100 p-2' >Submit</button>
-              </div>
-            </form>
+        <button onClick={() => setFormstate((pre) => !pre)} className='ps-2 pe-2 p-1 rounded bg-blue-500 text-white font-semibold m-3 cursor-pointer'>Add Blog</button>
+      </div>
+
+      <div>
+
+        {formstate && <div>
+          <form className='flex justify-center' onSubmit={handleForm}>
+            <div className='shadow-2xl shadow-gray-300 p-4 rounded space-y-4'>
+              <input className='border w-100 rounded-full p-2 ' value={title} onChange={(e) => setTitle(e.target.value)} required type="text" name="title" id="title" placeholder='Enter your title' /><br />
+              <input className='border w-100 rounded-full p-2 ' value={heading} onChange={(e) => setHeading(e.target.value)} required type="text" name="heading" id="" placeholder='Enter your heading' /><br />
+              <input className='border w-100 rounded-full p-2 '  onChange={handleImageChange} required type="file" name="img" accept="image/*"
+                id="" placeholder='Enter your title' /><br />
+              <input className='border w-100 rounded-full p-2 ' value={description} onChange={(e) => setDescription(e.target.value)} required name="description" id="" placeholder='Enter your description' ></input><br />
+              <button className='text-white font-semibold rounded-full bg-blue-500 w-100 p-2' >Submit</button>
+            </div>
+          </form>
         </div>}
 
-         <div className="p-4 mt-4  rounded-1">
-                            <h2 className="text-xl font-bold mb-4">Latest from Blog</h2>
-                            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                                {BlogData.map((item) => (
-                                    <LatestfromBlog key={item.id} item={item} />
-                                ))}
-                            </div>
-                        </div>
+        <div className="p-4 mt-4  rounded-1">
+          <h2 className="text-xl font-bold mb-4">Latest from Blog</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {BlogData.map((item) => (
+              <LatestfromBlog key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
